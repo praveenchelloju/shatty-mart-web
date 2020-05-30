2bdbaca4d6071f2a1191f6003e34f60661468348
pravee chelloju <51014301+praveenchelloju@users.noreply.github.com>
2020-05-31 00:06:15 +0530

    Add files via upload

--- assets/js/owlCarosel2.js
+++ assets/js/owlCarosel2.js
@@ -3,6 +3,10 @@ $(".owl-carousel").owlCarousel({
   loop: true,
   margin: 10,
   nav: true,
+  navText: [
+    "<div class='nav-btn prev-slide'></div>",
+    "<div class='nav-btn next-slide'></div>",
+  ],
   responsive: {
     0: {
       items: 1,
